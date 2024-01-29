3494078963b3583ed65c0b29490ffbda105a369b
Alex Khoroshev <metro3230@gmail.com>
2024-01-29 19:33:52 +0300

    Добавлен workdir

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from config import TOKEN
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
+workdir = os.getcwd()
 
 def add_to_log(val, path):                    #added value to log file if this value is not there 
     with open(path, 'r+') as file:
@@ -42,7 +42,7 @@ async def echo_message(msg: types.Message):
         await bot.send_message(usrid, 'нет')
     else:
         await bot.send_message(usrid, 'Я не дрочу на буквы!')
-    add_to_log(usrid, '/root/fapbot/log.txt')
+    add_to_log(usrid, workdir + '/log.txt')
     await bot.send_message(-931442766, f'Username: {msg.from_user.first_name}\nMsg: {text}\nId: {usrid}')   #отправляем во второй чят
 
 
@@ -52,21 +52,20 @@ async def get_photo(message):
     caption = message.caption
     first_name = message.from_user.first_name
 #    file_info = await bot.get_file(message.photo[-1].file_id)
-    await message.photo[-1].download(destination_file='/root/fapbot/photo/send-' + message.photo[-1].file_unique_id + '.jpg')
-    img = Image.open('/root/fapbot/photo/send-' + message.photo[-1].file_unique_id + '.jpg')
-    hand = Image.open('/root/fapbot/photo/hand.png') 
+    await message.photo[-1].download(destination_file=workdir + '/photo/send-' + message.photo[-1].file_unique_id + '.jpg')
+    img = Image.open(workdir + '/photo/send-' + message.photo[-1].file_unique_id + '.jpg')
+    hand = Image.open(workdir + '/photo/hand.png') 
     towidth = int(img.size[0])   #   До какой ширины растянуть руку.  0 - width   1 - height
     hsize = int((float(hand.size[1])*float(towidth/float(hand.size[0]))))                       # Формула подстройки высоты относительно нужной ширины
     hand = hand.resize((towidth,hsize), Image.Resampling.LANCZOS)                               #растягиваем
     zeropoint = int(img.size[1]) - hsize                                                        #начальная точка руки (что бы она была в левом нижнем углу)
     img.paste(hand, (0, zeropoint),  hand)                                                      #вставляем в нужное место
-    img.save('/root/fapbot/photo/send-' + message.photo[-1].file_unique_id + '_hand.jpg')       #временно сохраняем
-#    await bot.send_photo(-931442766, message.photo[-1].file_id, caption=message.from_user.first_name)           #отправляем в мой другой чят
+    img.save(workdir + '/photo/send-' + message.photo[-1].file_unique_id + '_hand.jpg')       #временно сохраняем
     await bot.send_photo(-931442766, message.photo[-1].file_id, caption=f'Username: {first_name}\nMsg: {caption}\nId: {usrid}')           #отправляем в мой другой чят
-    await bot.send_photo(usrid, types.InputFile('/root/fapbot/photo/send-' + message.photo[-1].file_unique_id + '_hand.jpg'))   #отправляем измененное 
-    add_to_log(usrid, '/root/fapbot/log.txt')
-    os.remove('/root/fapbot/photo/send-' + message.photo[-1].file_unique_id + '_hand.jpg')     #удаляем
-    os.remove('/root/fapbot/photo/send-' + message.photo[-1].file_unique_id + '.jpg')
+    await bot.send_photo(usrid, types.InputFile(workdir + '/photo/send-' + message.photo[-1].file_unique_id + '_hand.jpg'))   #отправляем измененное 
+    add_to_log(usrid, workdir + '/log.txt')
+    os.remove(workdir + '/photo/send-' + message.photo[-1].file_unique_id + '_hand.jpg')     #удаляем
+    os.remove(workdir + '/photo/send-' + message.photo[-1].file_unique_id + '.jpg')
 
 
 
